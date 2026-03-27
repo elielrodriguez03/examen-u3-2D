0fc1a0098fb6936c99d24d2018cf925a8203a095
dxemely <20253ds110@utez.edu.mx>
2026-03-27 08:54:00 -0600

    Nuevo proyecto Examen

--- proyecto-memoria-javafx-base-examen-2D-prestamo-llaves/src/main/java/com/example/prestamollaves/controller/MainController.java
+++ proyecto-memoria-javafx-base-examen-2D-prestamo-llaves/src/main/java/com/example/prestamollaves/controller/MainController.java
@@ -51,14 +51,15 @@ public class MainController {
 
     @FXML
     public void agregar() {
-        // TODO:
-        // 1. Leer txtNombreSolicitante, txtSalon y cbTurno.
-        // 2. Mandar esos datos al service.
-        // 3. Si el service regresa un mensaje, mostrar error.
-        // 4. Si regresa null, refrescar la lista y limpiar.
-        mostrarMensaje("Pendiente", "Completa la lógica de Agregar", Alert.AlertType.INFORMATION);
-    }
+        String error = service.agregar(txtNombreSolicitante.getText(), txtSalon.getText(), cbTurno.getValue());
+        if (error != null)
+        {
+            mostrarMensaje("Error", error, Alert.AlertType.INFORMATION);
+        } else {
+            actualizarLista();
 
+        }
+    }
     @FXML
     public void buscar() {
         // Método de ejemplo resuelto.
@@ -79,44 +80,55 @@ public class MainController {
 
     @FXML
     public void actualizar() {
-        // TODO:
-        // UPDATE reutiliza los mismos controles.
-        //
-        // Flujo esperado:
-        // 1. Primero buscar por nombre o seleccionar desde el ListView.
-        // 2. Eso debe cargar los datos en pantalla y guardar nombreOriginal.
-        // 3. Luego el usuario modifica txtNombreSolicitante, txtSalon y cbTurno.
-        // 4. Al presionar Actualizar, mandar al service:
-        //      - nombreOriginal
-        //      - txtNombreSolicitante.getText()
-        //      - txtSalon.getText()
-        //      - cbTurno.getValue()
-        // 5. El service debe buscar el registro original usando nombreOriginal.
-        // 6. Si lo encuentra, debe cambiar sus datos.
-        // 7. Luego refrescar el ListView y limpiar los controles.
-        //
-        // Importante:
-        // Si nombreOriginal es null, entonces no se ha buscado ni seleccionado nada.
-        mostrarMensaje("Pendiente", "Completa la lógica de Actualizar", Alert.AlertType.INFORMATION);
+        String error = service.actualizar(nombreOriginal, txtNombreSolicitante.getText(), txtSalon.getText(), cbTurno.getValue());
+        if (error != null) {
+            mostrarMensaje("Error", error, Alert.AlertType.INFORMATION);
+        } else {
+            actualizarLista();
+            limpiar();
+            // TODO:
+            // UPDATE reutiliza los mismos controles.
+            //
+            // Flujo esperado:
+            // 1. Primero buscar por nombre o seleccionar desde el ListView.
+            // 2. Eso debe cargar los datos en pantalla y guardar nombreOriginal.
+            // 3. Luego el usuario modifica txtNombreSolicitante, txtSalon y cbTurno.
+            // 4. Al presionar Actualizar, mandar al service:
+            //      - nombreOriginal
+            //      - txtNombreSolicitante.getText()
+            //      - txtSalon.getText()
+            //      - cbTurno.getValue()
+            // 5. El service debe buscar el registro original usando nombreOriginal.
+            // 6. Si lo encuentra, debe cambiar sus datos.
+            // 7. Luego refrescar el ListView y limpiar los controles.
+            //
+            // Importante:
+            // Si nombreOriginal es null, entonces no se ha buscado ni seleccionado nada.
+
+        }
     }
 
     @FXML
     public void eliminar() {
-        // TODO:
-        // DELETE sí borra el objeto de la lista.
-        //
-        // Flujo esperado:
-        // 1. Tomar el nombre desde txtNombreSolicitante.
-        // 2. Mandarlo al service.
-        // 3. El service debe buscarlo y eliminarlo de la lista.
-        // 4. Refrescar el ListView.
-        // 5. Limpiar controles.
-        //
-        // También se puede seleccionar un elemento del ListView
-        // y luego presionar Eliminar.
-        mostrarMensaje("Pendiente", "Completa la lógica de Eliminar", Alert.AlertType.INFORMATION);
+        String error = service.eliminar(txtNombreSolicitante.getText());
+        if (error != null) {
+            mostrarMensaje("Error", error, Alert.AlertType.INFORMATION);
+        } else {
+            actualizarLista();
+            //TODO:
+            // DELETE sí borra el objeto de la lista.
+            //
+            // Flujo esperado:
+            // 1. Tomar el nombre desde txtNombreSolicitante.
+            // 2. Mandarlo al service.
+            // 3. El service debe buscarlo y eliminarlo de la lista.
+            // 4. Refrescar el ListView.
+            // 5. Limpiar controles.
+            //
+            // También se puede seleccionar un elemento del ListView
+            // y luego presionar Eliminar.
+        }
     }
-
     @FXML
     public void limpiar() {
         txtNombreSolicitante.clear();
--- proyecto-memoria-javafx-base-examen-2D-prestamo-llaves/src/main/java/com/example/prestamollaves/service/PrestamoLlaveService.java
+++ proyecto-memoria-javafx-base-examen-2D-prestamo-llaves/src/main/java/com/example/prestamollaves/service/PrestamoLlaveService.java
@@ -27,37 +27,74 @@ public class PrestamoLlaveService {
     }
 
     public String agregar(String nombreSolicitante, String salon, String turno) {
+        if (nombreSolicitante==null||nombreSolicitante.trim().isEmpty()){
+            return "El nombre esta vacio";
+        }
+        if (salon==null||salon.trim().isEmpty()){
+            return "El salon esta vacio";
+        }
+        if (turno==null){
+            return "El turno esta vacio";
+        }
+        if (repository.buscarPorNombreSolicitante(nombreSolicitante.trim()) != null) {
+            return "Ya existe un registro";
+        }
+        repository.guardar(new PrestamoLlave(nombreSolicitante.trim(), salon.trim(), turno));
+        return null;
         // TODO:
-        // 1. Validar que nombreSolicitante no esté vacío.
-        // 2. Validar que salon no esté vacío.
-        // 3. Validar que turno no sea null.
-        // 4. Validar que no exista otro registro con el mismo nombreSolicitante.
-        // 5. Si todo está bien, crear un objeto PrestamoLlave y guardarlo en repository.
+        // 1. Validar que nombreSolicitante no esté vacío. ya
+        // 2. Validar que salon no esté vacío. ya
+        // 3. Validar que turno no sea null. ya
+        // 4. Validar que no exista otro registro con el mismo nombreSolicitante. yA
+        // 5. Si todo está bien, crear un objeto PrestamoLlave y guardarlo en repository. ya
         // 6. Regresar null cuando el registro se guarde correctamente.
-        return "Completa la lógica de agregar en el service";
     }
 
     public String actualizar(String nombreOriginal, String nombreNuevo, String salon, String turno) {
-        // TODO:
-        // 1. Validar que nombreOriginal no sea null ni vacío.
-        // 2. Validar que nombreNuevo no esté vacío.
-        // 3. Validar que salon no esté vacío.
-        // 4. Validar que turno no sea null.
-        // 5. Buscar el registro original usando nombreOriginal.
-        // 6. Si no existe, regresar mensaje de error.
-        // 7. Si el nombre cambió, validar que el nuevo nombre no esté repetido.
-        // 8. Si todo está bien, actualizar los atributos del objeto encontrado.
-        // 9. Regresar null si todo salió bien.
-        return "Completa la lógica de actualizar en el service";
+
+        if (nombreOriginal == null || nombreOriginal.isEmpty()) return "No hay un registro seleccionado para actualizar";
+
+        PrestamoLlave registro = repository.buscarPorNombreSolicitante(nombreOriginal);
+        if (registro == null) return "El registro original ya no existe";
+
+        if (!nombreOriginal.equalsIgnoreCase(nombreNuevo)) {
+            if (repository.buscarPorNombreSolicitante(nombreNuevo) != null) {
+                return "El nuevo nombre ya está registrado en otro préstamo";
+            }
+        }
+        registro.setNombreSolicitante(nombreNuevo);
+        registro.setSalon(salon);
+        registro.setTurno(turno);
+        return null;
     }
 
-    public String eliminar(String nombreSolicitante) {
         // TODO:
-        // 1. Validar que nombreSolicitante no esté vacío.
-        // 2. Buscar si existe el registro.
-        // 3. Si no existe, regresar mensaje de error.
-        // 4. Si existe, eliminarlo desde repository.
-        // 5. Regresar null si se eliminó correctamente.
-        return "Completa la lógica de eliminar en el service";
+        // 1. Validar que nombreOriginal no sea null ni vacío. ya
+        // 2. Validar que nombreNuevo no esté vacío. ya
+        // 3. Validar que salon no esté vacío. ya
+        // 4. Validar que turno no sea null.ya
+        // 5. Buscar el registro original usando nombreOriginal. ya
+        // 6. Si no existe, regresar mensaje de error.
+        // 7. Si el nombre cambió, validar que el nuevo nombre no esté repetido. ya
+        // 8. Si todo está bien, actualizar los atributos del objeto encontrado.
+        // 9. Regresar null si todo salió bien. ya
+
+    public String eliminar(String nombreSolicitante) {
+        if (nombreSolicitante == null || nombreSolicitante.isBlank()) return "Nombre inválido";
+        {
+
+            boolean ok = repository.eliminarPorNombreSolicitante(nombreSolicitante.trim());
+            return ok ? null : "No se pudo eliminar";
+        }
     }
 }
+
+
+        // TODO:
+        // 1. Validar que nombreSolicitante no esté vacío. ya
+        // 2. Buscar si existe el registro. ya
+        // 3. Si no existe, regresar mensaje de error. ya
+        // 4. Si existe, eliminarlo desde repository. ya
+        // 5. Regresar null si se eliminó correctamente ya
+
+
